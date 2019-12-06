26c28bd6144cd3fa6035e1e4c4b990ccf1e939cf
Andrei Zhydzenka <Andrei_Zhydzenka@epam.com>
2019-12-06 13:30:14 +0300

    Start creating a Breadcrumb component.

---
+++ src/Feature/Sitecore.Feature.Business/BreadcrumbBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Feature.Business.Models;
+using TAC.Sitecore.Abstractions.Interfaces;
+using TAC.Sitecore.Abstractions.SitecoreImplementation;
+
+namespace Sitecore.Feature.Business
+{
+    public class BreadcrumbBuilder
+    {
+        private readonly IRenderingContext _context;
+        public BreadcrumbBuilder() : this(SitecoreRenderingContext.Create()) { }
+        public BreadcrumbBuilder(IRenderingContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<NavigationItem> Build()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
