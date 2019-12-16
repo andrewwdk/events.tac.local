eb8f7ef7f1471ff330e321485b716f810a36f953
Andrei Zhydzenka <Andrei_Zhydzenka@epam.com>
2019-12-16 14:35:08 +0300

    Implement Main Navigation.

---
+++ src/Feature/Sitecore.Feature.Business/Controllers/MainNavigationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Feature.Business.Controllers
+{
+    public class MainNavigationController : Controller
+    {
+        private readonly MainNavigationBuilder _builder;
+
+        public MainNavigationController(MainNavigationBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public MainNavigationController() : this(new MainNavigationBuilder()) { }
+
+        // GET: MainNavigation
+        public ActionResult Index()
+        {
+            return PartialView("~/Views/MainNavigation/Index.cshtml", _builder.Build());
+        }
+    }
+}
---
+++ src/Feature/Sitecore.Feature.Business/Models/MainNavigationItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.Business.Models
+{
+    public class MainNavigationItem : NavigationMenuItem
+    {
+        public MainNavigationItem(string title, string url, IEnumerable<MainNavigationItem> children/*, bool excludeFromnavigation*/)
+            :base(title, url, children)
+        {
+        }
+    }
+}
