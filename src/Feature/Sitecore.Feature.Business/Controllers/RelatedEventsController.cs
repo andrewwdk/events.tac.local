73b6d10187c2ea7dd6258aec3f840b1e3127fb61
Andrei Zhydzenka <Andrei_Zhydzenka@epam.com>
2019-12-27 12:58:51 +0300

    Render complex field values.

---
+++ src/Feature/Sitecore.Feature.Business/Controllers/RelatedEventsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Feature.Business.Controllers
+{
+    public class RelatedEventsController : Controller
+    {
+        private readonly RelatedEventsProvider _provider;
+
+        public RelatedEventsController(RelatedEventsProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public RelatedEventsController() : this(new RelatedEventsProvider()) { }
+
+        // GET: RelatedEvents
+        public ActionResult Index()
+        {
+            return PartialView("~/Views/RelatedEvents/Index.cshtml", _provider.GetRelatedEvents());
+        }
+    }
+}
