3ba6bc1e7b3213a9aecf08aa332b3e5ccde96e8c
Andrei Zhydzenka <Andrei_Zhydzenka@epam.com>
2020-01-20 18:28:44 +0300

    IdentifyingaContact

--- src/Feature/Sitecore.Feature.Forms/Controllers/SubscribeFormController.cs
+++ src/Feature/Sitecore.Feature.Forms/Controllers/SubscribeFormController.cs
@@ -7,6 +7,9 @@ using TAC.Utils.Mvc;
 using Sitecore.Feature.Forms.Models;
 using Sitecore.Mvc.Presentation;
 using Sitecore.Web.UI.WebControls;
+using Sitecore.Analytics.Model.Entities;
+using Sitecore.Analytics.Outcome.Extensions;
+using Sitecore.Analytics;
 
 namespace Sitecore.Feature.Forms.Controllers
 {
@@ -28,6 +31,24 @@ namespace Sitecore.Feature.Forms.Controllers
         [ValidateFormHandler]
         public ActionResult Index(string email)
         {
+            string identificationSource = "events";
+            Sitecore.Analytics.Tracker.Current.Session.IdentifyAs(identificationSource, email);
+            var contact = Sitecore.Analytics.Tracker.Current.Contact;
+            var emails = contact.GetFacet<IContactEmailAddresses>("Emails");
+            if (!emails.Entries.Contains("personal"))
+            {
+                emails.Preferred = "personal";
+                var personalEmail = emails.Entries.Create("personal");
+                personalEmail.SmtpAddress = email;
+            }
+
+            //var outcome = new Sitecore.Analytics.Outcome.Model.ContactOutcome(
+            //    Sitecore.Data.ID.NewID,
+            //    new Data.ID("{EC97DE22-55CC-4A65-AACB-50435781D7D6}"),
+            //    new Data.ID(Tracker.Current.Contact.ContactId)
+            //    );
+            //Tracker.Current.RegisterContactOutcome(outcome);
+
             return PartialView("~/Views/SubscribeForm/Confirmation.cshtml");
         }
     }
