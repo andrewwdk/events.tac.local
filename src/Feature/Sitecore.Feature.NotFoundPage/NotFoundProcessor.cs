b5a0476335aa4a119180a25b30fc917ff80d675a
Andrei Zhydzenka <Andrei_Zhydzenka@epam.com>
2020-02-06 17:40:16 +0300

    Add 404 Not found pages.

---
+++ src/Feature/Sitecore.Feature.NotFoundPage/NotFoundProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Pipelines.HttpRequest;
+
+namespace Sitecore.Feature.NotFoundPage
+{
+    public class NotFoundProcessor : Sitecore.Pipelines.HttpRequest.HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            if (Sitecore.Context.Item != null || Sitecore.Context.Site == null || Sitecore.Context.Database == null)
+            { 
+                return;
+            }
+
+            var database = Sitecore.Context.Database;
+            var home = database.GetItem("/sitecore/content/events/home");
+            var notFoundPage = home?.GetChildren().First(x => x.TemplateID.ToString() == "{051FF4CA-456A-49A3-9840-C5DD919B4D1D}");
+            Sitecore.Context.Item = notFoundPage;
+        }
+    }
+}
